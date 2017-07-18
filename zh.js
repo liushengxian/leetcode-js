618878ce34069052ee9f1e0abf8b078a913ab156
liushengxian <liushengxian1230@163.com>
2017-07-18 17:10:11 +0800

    array n

--- zh.js
+++ zh.js
@@ -15,44 +15,85 @@
 
 // console.log(fn(3));
 
+// /**
+//  * @param {string[]} words
+//  * @return {string[]}
+//  */
+// var findWords = function(words) {
+//     let answer = [];
+//     let flag = true;
+//     let index = 0;
+//     let dictionary = ['qwertyuiop','asdfghjkl','zxcvbnm'];
+//     for(let word of words){
+//         console.log('word is '+ word);
+//         let lWord = word.toLowerCase();
+//         console.log('lWord is '+ lWord);
+//         for(let i = 0;i < 3;i++){
+//             if(dictionary[i].indexOf(lWord[0])!== -1){
+//                 index = i;
+//                 break;
+//             }
+//             console.log('i:'+ i);
+//             console.log(word);
+//         }
+        
+//         console.log('index:'+index);
+//         flag = true;
+//         for(let ix = 0;ix < lWord.length;ix++){
+//             if(dictionary[index].indexOf(lWord[ix])==-1){
+//                 flag = false;
+//                 break;
+//             }
+//         }
+        
+//         if(flag){
+//             answer.push(word);
+//         }
+        
+//     }
+    
+//     return answer;
+// };
+
+// console.log(findWords(['hello','alaska','dad','peace']));
+
 /**
- * @param {string[]} words
- * @return {string[]}
+ * @param {number[]} prices
+ * @return {number}
  */
-var findWords = function(words) {
-    let answer = [];
-    let flag = true;
-    let index = 0;
-    let dictionary = ['qwertyuiop','asdfghjkl','zxcvbnm'];
-    for(let word of words){
-        console.log('word is '+ word);
-        let lWord = word.toLowerCase();
-        console.log('lWord is '+ lWord);
-        for(let i = 0;i < 3;i++){
-            if(dictionary[i].indexOf(lWord[0])!== -1){
-                index = i;
-                break;
-            }
-            console.log('i:'+ i);
-            console.log(word);
+var maxProfit = function(prices) {
+    let buyIn = [];
+    let sellOut = [];
+    prices.unshift(0);
+    prices.push(0);
+
+    for(let ix in prices){
+        let index = parseInt(ix);
+        if(index===0||index===prices.length-1){
+            continue;
         }
-        
-        console.log('index:'+index);
-        flag = true;
-        for(let ix = 0;ix < lWord.length;ix++){
-            if(dictionary[index].indexOf(lWord[ix])==-1){
-                flag = false;
-                break;
-            }
+
+        if((prices[index] > prices[index-1])&&(prices[index] >= prices[index+1])){
+            sellOut.push(prices[index]);
+            console.log('enter 3');
         }
-        
-        if(flag){
-            answer.push(word);
+
+        if((prices[index] <= prices[index-1])&&(prices[index] < prices[index+1])){
+            buyIn.push(prices[index]);
+            console.log('enter 4');        
         }
-        
+    }
+    console.log(buyIn);
+    console.log(sellOut);
+
+    let answer = 0;
+    
+    for(let ix = 0 ;ix < buyIn.length; ix++){
+        // answer *= ((sellOut[ix] - buyIn[ix])/buyIn[ix]);
+        answer += ((sellOut[ix] - buyIn[ix]));
     }
     
     return answer;
 };
 
-console.log(findWords(['hello','alaska','dad','peace']));
+console.log(maxProfit([2,2,5]));
